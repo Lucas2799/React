b86c19c908eead114891d8f19651d8d748b47e4e
Lucas Tavares <ltv2234@gmail.com>
2020-10-12 16:23:59 -0300

    incrementado horas

--- src/Contador.js
+++ src/Contador.js
@@ -9,40 +9,54 @@ class Contador extends React.Component{
     constructor(props){
     super(props)
     this.state = {
-        segundos : 0,
-        minutos : 0,
+        segundo : 0,
+        minuto : 0,
+        hora: 0,
         stop: false,
         nameStop: "Stop",
         parcial: " "
     }
 
+
 }
 incrementar(){
     this.setState(
         (state) =>{
-            if(this.state.stop==false){
-                if (state.segundos >=100){
+            if(this.state.stop === false){
+                if (state.segundo >= 60){
                     this.zerar();
                     this.incrementarMinutos()
                 } 
-                return({segundos: state.segundos + 1})
+                if (state.minuto === 60){
+                    this.incrementarHoras()
+                    this.zerar();
+                } 
+               
+                return({segundo: state.segundo + 1})
         }
         }
     )
 }
 
 zerar(){
-    this.setState({segundos: 0})
+    this.setState({segundo: 0})
+    this.setState({minuto: 0})
+    
 }
 
 incrementarMinutos(){
-    this.setState({minutos: this.state.minutos + 1})
+    this.setState({minuto: this.state.minuto + 1})
+}
+
+incrementarHoras(){
+    this.setState({hora: this.state.hora + 1})
 }
 
 zerarCronometro(){
     this.setState({
-        segundos: 0,
-        minutos: 0
+        segundo: 0,
+        minuto: 0,
+        hora : 0
     })
 }
 
@@ -62,7 +76,7 @@ pararTempo()
         
 }
 parciais(){
-    let p = this.state.minutos +":" + this.state.segundos
+    let p =  this.state.hora +":" +this.state.minuto + ":" + this.state.segundo
         this.setState({
             parcial : this.state.parcial + "->" + p
         })
@@ -70,7 +84,7 @@ parciais(){
  
 
 componentDidMount(){
-    this.timer =  setInterval( () => this.incrementar(), 10)
+    this.timer =  setInterval( () => this.incrementar(), 1000)
 }
 
 
@@ -79,7 +93,7 @@ componentDidMount(){
  render(){
     return(
         <div>
-            <h1>{this.state.minutos}:{this.state.segundos}</h1>
+            <h1>{this.state.hora}:{this.state.minuto}:{this.state.segundo}</h1>
             <Botao onClick ={()=> { this.zerarCronometro()}} label ="Zerrar"/>
             <Botao onClick ={()=> { this.pararTempo()}} label ={this.state.nameStop}/>
             <Botao onClick ={()=> { this.parciais()}} label ="Parcial"/>
