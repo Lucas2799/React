078040299fea45af29c0b603e79874b9e8a2d5c6
lucas2799 <ltv2234@gmail.com>
2023-04-29 23:40:49 -0300

    fix

---
+++ src/comp/pges/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './sub/Home'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Home/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/comp/pges/sub/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+
+class Home extends React.Component{
+ render(){
+     return (
+         <h1>
+             Reestruturando page
+         </h1>
+    )
+}
+
+    
+}
+
+export default Home;
